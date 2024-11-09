d0d2c5e9135e84e666b0b19b8139c0ddb79e4bac
Mayank  Keshari <mayank.keshari.93@gmail.com>
2024-11-09 23:58:06 +0530

    sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
+import Sidebar from '../components/Sidebar';
 import './App.css'
+import { useState } from 'react';
 
 function App() {
 
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  // Toggle function for sidebar
+  const onSetSidebarOpen = (open) => {
+    setSidebarOpen(open);
+  };
+
   return (
     <>
-
+    <Sidebar/>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
