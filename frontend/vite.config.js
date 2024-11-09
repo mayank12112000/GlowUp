2f58bc1a5256652b86345413d7bf94e49cd2b4b2
Mayank  Keshari <mayank.keshari.93@gmail.com>
2024-11-09 20:23:05 +0530

    update config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,19 @@
+import path from 'path';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()],resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   server: {
     host: '0.0.0.0', // This allows access from your local network
     port: 3000,      // You can use any port you like
+  },
+  optimizeDeps: {
+    exclude: ['lucide-react'],
   },
 })
