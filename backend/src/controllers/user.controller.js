7e7db4d07e8de97337b2c8b0320e2c780815c958
Mayank  Keshari <mayank.keshari.93@gmail.com>
2024-11-21 21:18:06 +0530

    minor adjustment

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -7,24 +7,19 @@ import bcrypt from "bcrypt"
 const registerUser = asyncHandler(async (req,res)=>{
     const {name,mobile,roleSeq,userName,email,password} = req.body
 
-    if (!name || !mobile || !password || !userName || !roleSeq || !email) {
+    if ([name, mobile, password, userName, roleSeq, email].some(field => !field)) {
         throw new ApiError(400, "All fields are required");
     }
-    
-
     // check if users already exits
     const {results} = await runQuery(SELECT_USER,[userName,email,mobile])
     if(results?.length > 0){
         throw new ApiError(403,"user name/email id/mobile number already exists")
     }
-
     // hash the password
     const hashedPassword = await bcrypt.hash(password,10)
 
     // insert into users table
-    // "INSERT INTO USERS (USERNAME,EMAIL,PASSWORD,NAME,MOBILE,ROLE_SEQ,CREATED_AT,UPDATED_AT) VALUES (?,?,?,?,?,?,NOW(),NOW())"
-
-    const result = await runQuery(CREATE_USER,[userName.toLowerCase(),email,hashedPassword,name,mobile,roleSeq])
+    await runQuery(CREATE_USER,[userName.toLowerCase(),email,hashedPassword,name,mobile,roleSeq])
     
     // after successfully insertion return apiresponse
     return res.status(201).json(new ApiResponse(
