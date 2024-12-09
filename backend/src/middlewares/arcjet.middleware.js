3c579b1f0e521b398518ae8a7878c3e2cb6ff2c2
Mayank  Keshari <mayank.keshari.93@gmail.com>
2024-12-09 22:12:50 +0530

    arcject disposable email validation

---
+++ backend/src/middlewares/arcjet.middleware.js
@@ -0,0 +1,45 @@
+import arcjet, { validateEmail } from "@arcjet/node";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+
+const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  rules: [
+    validateEmail({
+      mode: "LIVE", 
+      block: ["DISPOSABLE", "INVALID", "NO_MX_RECORDS"],
+    }),
+  ],
+});
+
+export const arcjectValidate = asyncHandler(async (req,_,next) => {
+    const {email} = req.body
+  const decision = await aj.protect(req, {
+    email: email,
+  });
+  
+  let message = "";
+  // also reveal the validation to a spammer
+  if (decision.reason.emailTypes.includes("DISPOSABLE")) {
+    message = "We do not allow disposable email addresses.";
+  } else if (decision.reason.emailTypes.includes("FREE")) {
+    message =
+      "We do not allow free email addresses, please use a business address.";
+  } else if (decision.reason.emailTypes.includes("NO_MX_RECORDS")) {
+    message = "Your email domain does not have an MX record. Is there a typo?";
+  } else if (decision.reason.emailTypes.includes("NO_GRAVATAR")) {
+    message = "We require a Gravatar profile to sign up.";
+  } else {
+    message = "Invalid email.";
+  }
+  
+  if (decision.isDenied()) {
+    // res.writeHead(403, { "Content-Type": "application/json" });
+    console.log(decision.reason)
+    // res.end(JSON.stringify({ error: "Forbidden" }));
+    throw new ApiError(403,message)
+
+
+  } 
+  next()
+});
--- backend/src/routes/user.router.js
+++ backend/src/routes/user.router.js
@@ -1,10 +1,11 @@
 import {Router} from "express";
 import { currentUserRole, getOtp, loginUser, logoutUser, registerUser, validateUser, verifyOtp } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { arcjectValidate } from "../middlewares/arcjet.middleware.js";
 
 const router = Router()
 
-router.route("/register").post(registerUser)
+router.route("/register").post(arcjectValidate,registerUser)
 router.route("/login").post(loginUser)
 router.route("/validate").get(validateUser)
 router.route("/currentRole").get(verifyJWT,currentUserRole)
