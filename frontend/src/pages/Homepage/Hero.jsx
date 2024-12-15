23e2facd10629b6a97032a741228d88b83049bae
Mayank  Keshari <mayank.keshari.93@gmail.com>
2024-12-16 01:10:23 +0530

    hero banner change

--- frontend/src/pages/Homepage/Hero.jsx
+++ frontend/src/pages/Homepage/Hero.jsx
@@ -1,16 +1,26 @@
 import React from "react";
+import "./Hero.css"; // Import the styles
 
 export default function Hero() {
   return (
-    <div className="grid-container">
-      <div className="grid-item item-1 m-0">
-        <img className="img m-0" src="fairone/dp4.png" alt="Image 1" />
+    <div className="hero-container">
+      <div className="hero-banner d-flex align-items-center">
+        <img src="fairone/dp1.png" alt="Hero Banner" className="hero-banner__img" />
+        <div className="hero-overlay">
+          <h1 className="hero-title">Welcome to FairOne</h1>
+        </div>
       </div>
-      <div className="grid-item item-2 m-3">
-        <img className="img m-0" src="fairone/dp2.png" alt="Image 2" />
-      </div>
-      <div className="grid-item item-3 m-3">
-        <img className="img m-0" src="fairone/dp3.png" alt="Image 3" />
+
+      <div className="hero-grid">
+        <div className="hero-grid__item">
+          <img src="fairone/dp2.png" alt="Image 1" className="hero-grid__img" />
+        </div>
+        <div className="hero-grid__item">
+          <img src="fairone/dp3.png" alt="Image 2" className="hero-grid__img" />
+        </div>
+        <div className="hero-grid__item">
+          <img src="fairone/dp4.png" alt="Image 3" className="hero-grid__img" />
+        </div>
       </div>
     </div>
   );
