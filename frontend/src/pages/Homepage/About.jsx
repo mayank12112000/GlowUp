0372dcacf624c1dd26eaa7a8d1db54727b0de80c
Mayank  Keshari <mayank.keshari.93@gmail.com>
2024-12-16 01:22:35 +0530

    photo changed

--- frontend/src/pages/Homepage/About.jsx
+++ frontend/src/pages/Homepage/About.jsx
@@ -8,7 +8,8 @@ export default function About() {
      <a href="https://www.google.com/maps/place/Fair+1+Makeovers/@24.0242736,81.3753453,17z/data=!4m6!3m5!1s0x39842f1949fd00a9:0xac7d977bf701be22!8m2!3d24.0242687!4d81.3779256!16s%2Fg%2F11b7vzwmfk?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D" 
      target="_blank" aria-label="Get directions"><div className="Visibility_gte-tablet__OUjR7" id=":r23u:"><picture className="rC5_pk _-rO_ck oN-olk">
         <img style={{width:"98%"}} src="fairone/location.png" alt="Tiara East Tower, Business Bay, Dubai" className="glocation" loading="lazy"/></picture></div><div className="Visibility_lt-tablet__FVQew" id=":r23v:"></div></a>
-        <p className='my-3 fs-5'>Hanuman Ganj, Beohari <a target='_blank' href="https://www.google.com/maps/place/Fair+1+Makeovers/@24.0242736,81.3753453,17z/data=!4m6!3m5!1s0x39842f1949fd00a9:0xac7d977bf701be22!8m2!3d24.0242687!4d81.3779256!16s%2Fg%2F11b7vzwmfk?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D"> Get Direction</a> </p>
+        <p className='my-3 fs-5'>Hanuman Ganj, Beohari <a target='_blank' href="https://www.google.com/maps/place/Fair+1+Makeovers/@24.0242736,81.3753453,17z/data=!4m6!3m5!1s0x39842f1949fd00a9:0xac7d977bf701be22!8m2!3d24.0242687!4d81.3779256!16s%2Fg%2F11b7vzwmfk?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D">
+        <br /> Get Direction</a> </p>
     </>
   )
 }
